00a9c8515d3540fcd551268a166ad97c4204d9cc
Sarah Kashin <33205001+skashin95@users.noreply.github.com>
2017-12-05 15:13:05 -0500

    Completed class

--- VehicleDefence/VehicleDefence/Model/ShotMovedEventArgs.cs
+++ VehicleDefence/VehicleDefence/Model/ShotMovedEventArgs.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace VehicleDefence.Model
 {
-    class ShotMovedEventArgs
+    using Windows.Foundation;
+    
+    class ShotMovedEventArgs : EventArgs
     {
+        public Shot Shot { get; private set; }
+        public bool Disappeared  { get; private set; }
+        
+        pubic ShotMovedEventArgs (Shot shot, bool disappeared0
+        {
+            Shot = shot;
+            Disappeared = disappeared;
+            
+        }
     }
 }
