79eb096a2afafa08a526b4a329fbdc603a7aa146
Sarah Kashin <33205001+skashin95@users.noreply.github.com>
2017-12-05 15:07:45 -0500

    Defined variables and wrote methods

--- VehicleDefence/VehicleDefence/Model/Aircraft.cs
+++ VehicleDefence/VehicleDefence/Model/Aircraft.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace VehicleDefence.Model
 {
-    class Aircraft
+    using Windows.Foundation;
+    
+    class Aircraft : MilitaryVehicle
     {
+        public static readonly Size AircraftSize = new Size(15, 15);
+        public const int HorizontalInterval = 5;
+        public const int VerticalInterval = 15;
+        
+        public AircraftType AircraftType { get; private set; }
+        public int Score { get; private set; }
+        
+        public Aircraft (AircraftType aircraftType, Point location, int score) : base(location, Aircraft.AircraftSize)
+        {
+            this.AircraftType = aircraftType;
+            this.Score = score;
+        }
+        
+        public override Move (Direction aircraftDirection)
+        {
+            switch (aircraftDirection)
+                {
+                    case Direction.Right:
+                        Location = new Point(Location.X + HorizontalInterval, Location.Y);
+                        break;
+                        
+                    case Direction.Left:
+                        Location = new Point(Location.X - HorizontalInterval, Location.Y);
+                        break;                        
+                        
+                    default:
+                        Location = new Point(Location.X, Location.Y + VerticalInterval);
+                        break;
+                }       
+        }
+        
     }
 }
